cc9b10d08c4a684f57003c40b0b3a21b28378dbe
Frederik Batuna <gobi301@gmail.com>
2019-02-13 18:00:44 +0100

    add: utils actionOfType, runActionsOnReducer

--- src/utility.ts
+++ src/utility.ts
@@ -8,13 +8,6 @@ export type Reducer<State, Action extends { type: string; payload?: any }> = (
 export type ActionOfReducer<R extends Reducer<any, any>> = Parameters<R>[1];
 export type StateOfReducer<R extends Reducer<any, any>> = ReturnType<R>;
 
-export function ignore<State>(state: State) {
-  return state;
-}
-export function get<Attribute extends string>(attribute: Attribute) {
-  return <Value>(object: { [K in Attribute]: Value }) => object[attribute];
-}
-
 export interface DeepReadonlyArray<A> extends ReadonlyArray<DeepReadonly<A>> {}
 
 export type DeepReadonlyObject<A> = {
@@ -24,3 +17,24 @@ export type DeepReadonlyObject<A> = {
 export type DeepReadonly<A> = A extends Array<infer B>
   ? DeepReadonlyArray<B>
   : DeepReadonlyObject<A>;
+
+export function ignore<State>(state: State) {
+  return state;
+}
+
+export function get<Attribute extends string>(attribute: Attribute) {
+  return <Value>(object: { [K in Attribute]: Value }) => object[attribute];
+}
+
+export function actionOfType<ActionType extends string>(
+  actionType: ActionType
+) {
+  return <Payload>(payload: Payload) => ({ type: actionType, payload });
+}
+
+export function runActionsOnReducer<
+  State,
+  Action extends { type: string; payload?: any }
+>(reducer: Reducer<State, Action>, state: State, actions: Action[]) {
+  return actions.reduce(reducer, state);
+}
